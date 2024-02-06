6fd5dfbd68e1a5d07ea330f2eacfd26155f007d3
Yulai202020 <ngmyulai@gmail.com>
2024-02-06 21:24:24 +0500

    first commit

---
+++ publish.sh
@@ -0,0 +1,7 @@
+rm -rf dist
+rm -rf build
+
+python -m build --sdist .
+python -m build --wheel .
+
+python -m twine upload dist/*
---
+++ yulain_test_project/__main__.py
@@ -0,0 +1,5 @@
+import yulain_test_project.test as test
+
+if __name__ == "__main__":
+    hello = test.helloworld()
+    print(hello)
